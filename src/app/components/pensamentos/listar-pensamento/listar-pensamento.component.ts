99a4e4df7cfee8487a83b048cc68662238c3897b
Pedro Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2023-05-27 23:37:24 -0300

    feat: add pagination in project and make some changes

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,16 +5,30 @@ import { PensamentoService } from '../pensamento.service';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
   constructor(private service: PensamentoService) {}
 
   listaPensamentos: Pensamento[] = [];
 
+  paginaAtual: number = 1;
+
+  maisPensamentos: boolean = true;
+
   ngOnInit(): void {
-      this.service.listar().subscribe((listaPensamentos) => {
-        this.listaPensamentos = listaPensamentos;
-      })
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos;
+    });
   }
+
+  carregarMaisPensamentos = () => {
+    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
+
+      if (!listaPensamentos.length) {
+        this.maisPensamentos = false;
+      }
+    });
+  };
 }
